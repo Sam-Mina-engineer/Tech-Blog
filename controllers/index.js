8a83cf940ec78e0f547104f405f088ece033403c
Sam Mina <samandmissy2010@gmail.com>
2024-11-24 21:33:48 -0500

    Updated routes in controllers index js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const withAuth = require('../config/auth');
 
 // Import the API routes
 
 router.use('/api', apiRoutes);
+
+// Route to render the signup page
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+// Route to render the homepage
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+// Route to render the login page
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Route to render the dashboard page
+
+router.get('/dashboard', withAuth, (req, res) => {
+  res.render('dashboard');
+});
 
 // Handle requests to wrong routes
 
@@ -12,4 +37,3 @@ router.use((req, res) => {
 });
 
 module.exports = router;
-
