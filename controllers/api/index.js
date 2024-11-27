b7d8e5217fcbbba999aee0d202ac1fc425c769c8
Sam Mina <samandmissy2010@gmail.com>
2024-11-26 23:50:06 -0500

    Fixed the indexes and their directories whew

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,40 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const withAuth = require('../config/auth');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
 
-// Import the API routes
+// Specific routes
 
-router.use('/api', apiRoutes);
-
-// Route to render the signup page
-
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-// Route to render the homepage
-
-router.get('/', (req, res) => {
-  res.render('homepage');
-});
-
-// Route to render the login page
-
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-
-// Route to render the dashboard page
-
-router.get('/dashboard', withAuth, (req, res) => {
-  res.render('dashboard');
-});
-
-// Handle requests to wrong routes
-
-router.use((req, res) => {
-  res.status(404).send('<h1>404 - Page Not Found</h1>');
-});
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
